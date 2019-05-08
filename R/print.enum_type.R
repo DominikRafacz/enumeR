6253406aad4c582de7b2b8abd280c67b721aeb4e
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-05-08 19:27:41 +0200

    first functions

---
+++ R/print.enum_type.R
@@ -0,0 +1,11 @@
+#'  @title Print enum_type
+#'
+#'  @description Pretty prints enum_type object. Overloads default print.
+#'
+#'  @param enum_type Object of class enum_type
+
+print.enum_type <- function(enum_type) {
+  cat("Enum type: ", enum_type$type_name,
+      "\nValues:\n")
+  print(enum_type$values)
+}
