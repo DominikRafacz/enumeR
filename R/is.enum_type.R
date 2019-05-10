39d0d3ef010fcd26ac316aa8d67213693ea0e95a
MatiFilozof <matibakala@gmail.com>
2019-05-10 16:22:05 +0200

    is.enum_type description

--- R/is.enum_type.R
+++ R/is.enum_type.R
@@ -1,12 +1,18 @@
-#'  @title TEMPORARRY DOCUMENTATION
+#'  @title Test if the object is an enum type
 #'
-#'  @description TEMPORARY DOCUMENTATION
+#'  @description This function checks class of passed object. If vector of classes contains
+#'  \code{"enum_type"}, then checks if all necessary fields are not \code{null}, namely
+#'  \code{type_name} and \code{values}, and whether \code{type_name} is a singular
+#'  character vector. Next, checks if \code{fields_names} is a character vector or \code{null}.
+#'  Lastly, all values need to be unique and proper \code{enum_value} objects.
+#'
+#'  @return \code{TRUE} if the object passed all tests described above, \code{FALSE} otherwise.
 #'
 #'  @export
 
 is.enum_type <- function(object) {
   "$" <- base::"$"
-  if(class(object) == "enum_type" &&
+  if("enum_type" %in% class(object) &&
      !any(is.null(object$type_name),
           is.null(object$values)) &&
      is.character(object$type_name) &&
