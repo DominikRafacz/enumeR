a703e4768a6cd9cfca9b163f6942c65a56011b1e
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-05-14 22:35:40 +0200

    added vectorization for get_enum_index

--- R/enum_index.R
+++ R/enum_index.R
@@ -2,13 +2,17 @@
 #'
 #'  @description Extracts `index` attribute value of passed `enum_value`.
 #'
-#'  @param enum_value object of class `enum_value`
+#'  @param enum_value object of class `enum_value` or list of such objects with the same `enum_type`
 #'
-#'  @return integer index of
+#'  @return integer index of `enum_value` or named vector of integers coresponding to  values in list
 #'
 #'  @export
 
 get_enum_index <- function(enum_value) {
-  stopifnot(is.enum_value(enum_value))
-  enum_value$index
+  stopifnot(is.enum_value(enum_value) || (is.list(enum_value) && all(sapply(enum_value, is.enum_value))))
+  if(is.enum_value(enum_value)) {
+    enum_value$index
+  } else {
+    sapply(enum_value, function(value) value$index)
+  }
 }
